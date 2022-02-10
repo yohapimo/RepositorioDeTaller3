01b3dbb759912c57e8fa36c3d291e957ad43ba57
Yon Hawer Piñeros <96067867+yohapimo@users.noreply.github.com>
2022-02-09 20:53:13 -0500

    Actualizaciones en Taller 3
    Se realiza primer actualización del taller 3

--- pooProject3/src/pooproject3/PooProject3.java
+++ pooProject3/src/pooproject3/PooProject3.java
@@ -15,7 +15,9 @@ public class PooProject3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Se realiza el primer cambio");
+        
     }
     
 }
